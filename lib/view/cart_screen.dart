eb3f15af8ca3b461745a0398fb25a8697427f900
MohamedHasan <muhamedmustafa3.14@gmail.com>
2021-09-16 00:43:27 +0200

    cart screen image

--- lib/view/cart_screen.dart
+++ lib/view/cart_screen.dart
@@ -11,111 +11,129 @@ class CartScreen extends StatelessWidget {
     return GetBuilder<CartViewModel>(
         init: CartViewModel(),
         builder: (controller) {
-          return Column(
-            children: [
-              Expanded(
-                  child: ListView.separated(
-                separatorBuilder: (context, index) => Divider(),
-                itemCount: controller.cartProducts.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 140,
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 170,
-                          child: Image.network(
-                            controller.cartProducts[index].image!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 30),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+          return controller.cartProducts.length == 0
+              ? Center(
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/empty.png'),
+                    SizedBox(height: 50),
+                    CustomText(
+                      text: 'Empty Cart !',
+                      size: 28,
+                      color: primaryColor,
+                      alignment: Alignment.center,
+                    ),
+                  ],
+                ))
+              : Column(
+                  children: [
+                    Expanded(
+                        child: ListView.separated(
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: controller.cartProducts.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 140,
+                          child: Row(
                             children: [
-                              CustomText(
-                                text: controller.cartProducts[index].name,
-                                size: 24,
-                              ),
-                              SizedBox(height: 10),
-                              CustomText(
-                                text: controller.cartProducts[index].price,
-                                color: primaryColor,
-                              ),
-                              SizedBox(height: 20),
                               Container(
-                                color: Colors.grey.shade200,
-                                child: Row(
+                                width: 170,
+                                child: Image.network(
+                                  controller.cartProducts[index].image!,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 30),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    IconButton(
-                                        onPressed: () {
-                                          controller.increaseQuantity(index);
-                                        },
-                                        icon: Icon(Icons.add)),
-                                    SizedBox(width: 20),
                                     CustomText(
-                                        text: controller
-                                            .cartProducts[index].quantity
-                                            .toString()),
-                                    SizedBox(width: 20),
-                                    IconButton(
-                                        onPressed: () {
-                                          controller.decreaseQuantity(index);
-                                        },
-                                        icon: Icon(Icons.remove)),
+                                      text: controller.cartProducts[index].name,
+                                      size: 24,
+                                    ),
+                                    SizedBox(height: 10),
+                                    CustomText(
+                                      text:
+                                          controller.cartProducts[index].price,
+                                      color: primaryColor,
+                                    ),
+                                    SizedBox(height: 20),
+                                    Container(
+                                      color: Colors.grey.shade200,
+                                      child: Row(
+                                        children: [
+                                          IconButton(
+                                              onPressed: () {
+                                                controller
+                                                    .increaseQuantity(index);
+                                              },
+                                              icon: Icon(Icons.add)),
+                                          SizedBox(width: 20),
+                                          CustomText(
+                                              text: controller
+                                                  .cartProducts[index].quantity
+                                                  .toString()),
+                                          SizedBox(width: 20),
+                                          IconButton(
+                                              onPressed: () {
+                                                controller
+                                                    .decreaseQuantity(index);
+                                              },
+                                              icon: Icon(Icons.remove)),
+                                        ],
+                                      ),
+                                    )
                                   ],
                                 ),
                               )
                             ],
                           ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              )),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomText(
-                          text: 'TOTAL',
-                          size: 22,
-                          color: Colors.grey,
-                        ),
-                        SizedBox(height: 15),
-                        CustomText(
-                          text: '\$' + controller.totalPrice.toString(),
-                          size: 18,
-                          color: primaryColor,
-                        ),
-                      ],
-                    ),
-                    Container(
-                      width: 180,
-                      height: 70,
-                      child: CustomButton(
-                        onTap: () {
-                          //TODO
-                        },
-                        child: CustomText(
-                          text: 'CHECK OUT',
-                          alignment: Alignment.center,
-                          color: Colors.white,
-                        ),
+                        );
+                      },
+                    )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomText(
+                                text: 'TOTAL',
+                                size: 22,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(height: 15),
+                              CustomText(
+                                text: '\$' + controller.totalPrice.toString(),
+                                size: 18,
+                                color: primaryColor,
+                              ),
+                            ],
+                          ),
+                          Container(
+                            width: 180,
+                            height: 70,
+                            child: CustomButton(
+                              onTap: () {
+                                //TODO
+                              },
+                              child: CustomText(
+                                text: 'CHECK OUT',
+                                alignment: Alignment.center,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                     )
                   ],
-                ),
-              )
-            ],
-          );
+                );
         });
   }
 }
